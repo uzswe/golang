e1bb33e7222ca57278e1f2644db587f0b634fbfb
Xojiakbar Isroilov <uer.oooouuuuooo@gmail.com>
2025-02-01 21:46:14 +0500

    intro lesson changed

--- #1-intro/index.go
+++ #1-intro/index.go
@@ -1,23 +1,19 @@
 /*
-* This Go program prints the message "Hello, World!" to the terminal.
+* This GO program prints the message "Hello, World" to the terminal.
 * This program is a basic example for beginners.
 *
 * Basic steps:
 * 1. Define the main package with `package main`.
 * 2. Call the formatting library with `import "fmt"`.
-* 3. Create the `func main()` function — the starting point of Go programs.
-* 4. "var" is a special keyword for creating a new variable.
-* 5. world is the name of this variable to always call it, and string is the type name indicating that the value of this variable is of type text
-* 6. Print text to the terminal with `fmt.Println()`.
+* 3. Create the `func main()` function — the starting point of GO programs.
+* 4. Print text to the terminal with `fmt.Printf()`.
 *
 * We used fmt in this program because fmt is a special package for input/output operations.
 * Actually this program is very simple, you can try to make this program yourself.
 *
 * Find answers to specific questions about the program:
 * 1. Why are we calling and using the main package in this program?
-* 2. What is the difference println() and fmt.Println()
-* 3. Can we change the main function name to myMain?
-* 4. Can we change the type of world variable to bool?
+* 2. Can we change the main function name to myMain?
 *
  */
 
@@ -26,7 +22,6 @@ package main
 import "fmt"
  
  func main() {
-	 var world string = "World"
-	 fmt.Println("Hello", world)
+	 fmt.Printf("Hello World")
  }
  
